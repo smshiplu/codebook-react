a1dec021c7769508c91a64475a441997c0b8dead
SM-Nasir-Uddin_Shiplu <nasiruddinshiplu@gmail.com>
2023-10-06 18:50:44 +0600

    ENV Refresh

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,6 +2,7 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { useDocTitle } from "../hooks/useDocTitle";
 import { register } from "../services";
+
 export const Register = () => {
   useDocTitle("Registration");
   const navigate = useNavigate();
